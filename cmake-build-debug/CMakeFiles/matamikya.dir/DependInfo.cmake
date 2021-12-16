d3231ee9d6363fc3cab317528bf20de96e3f100c
Noam Marco <noam0711marko@gmail.com>
2021-12-16 10:53:12 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/matamikya.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.qj8EqPYyil/biadsy_matamikya_tests.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/biadsy_matamikya_tests.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya_orders.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya_orders.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya_orders_set.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya_orders_set.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya_print.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya_print.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya_product_in_order.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya_product_in_order.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya_products.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya_products.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya_products_set.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya_products_set.c.o"
+  "/tmp/tmp.qj8EqPYyil/matamikya_validation_check.c" "/tmp/tmp.qj8EqPYyil/cmake-build-debug/CMakeFiles/matamikya.dir/matamikya_validation_check.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ cmake-build-debug/CMakeFiles/matamikya.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/matamikya.dir/biadsy_matamikya_tests.c.o"
+  "CMakeFiles/matamikya.dir/matamikya.c.o"
+  "CMakeFiles/matamikya.dir/matamikya_print.c.o"
+  "CMakeFiles/matamikya.dir/matamikya_validation_check.c.o"
+  "CMakeFiles/matamikya.dir/matamikya_products.c.o"
+  "CMakeFiles/matamikya.dir/matamikya_products_set.c.o"
+  "CMakeFiles/matamikya.dir/matamikya_product_in_order.c.o"
+  "CMakeFiles/matamikya.dir/matamikya_orders.c.o"
+  "CMakeFiles/matamikya.dir/matamikya_orders_set.c.o"
+  "matamikya.pdb"
+  "matamikya"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/matamikya.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
